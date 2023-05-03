a9754f163f4f54f00506350dc909433e6a99f221
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-05-03 17:01:29 +0600

    header style connected and components creation done

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>react country in other file
+            </h3>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Users/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h3>Users from components</h3>
+        </div>
+    );
+};
+
+export default Users;
